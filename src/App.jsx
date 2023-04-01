c49ea4321c60769464b969136b02f258297901bd
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2023-04-01 16:03:04 -0400

    switch to browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { Route, createHashRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import Dunno from './Dunno';
 import Layout from './Layout';
 import Users from './Users';
 
-const router = createHashRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
